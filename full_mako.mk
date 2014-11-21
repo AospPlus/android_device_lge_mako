52d7009bf487b122dbd9ab4f078df980d841af82
Tom Briden <tom@decompile.me.uk>
2014-11-21 15:32:51 +0000

    Changed to go with nexus binaries make file

--- full_mako.mk
+++ full_mako.mk
@@ -38,4 +38,4 @@ PRODUCT_MANUFACTURER := LGE
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/mako/device.mk)
-$(call inherit-product-if-exists, vendor/lge/mako/mako-vendor.mk)
+$(call inherit-product, vendor/lge/mako/device-vendor.mk)
